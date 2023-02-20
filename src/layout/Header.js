2b0297919931a0b02a37e0420483c45d97cbee4b
whomohitgoyal <mohit@droot.in>
2023-02-20 10:24:47 +0530

    -- changed menu stying

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -1,40 +1,41 @@
 import Link from "next/link";
 import { useEffect, useState } from "react";
-import Image from 'next/image';
-import drootLogoPic from '../../public/assets/images/drootlogoW.png';
+import Image from "next/image";
+import drootLogoPic from "../../public/assets/images/drootlogoW.png";
+import ReactGA from 'react-ga';
+
 function useScrollDirection() {
   const [scrollDirection, setScrollDirection] = useState(null);
 
   useEffect(() => {
-      let lastScrollY = window.pageYOffset;
-      // function to run on scroll
-      const updateScrollDirection = () => {
-          const scrollY = window.pageYOffset;
-          const direction = lastScrollY > 80 ? "down" : "up";
-          if (direction !== scrollDirection) {
-            setScrollDirection(direction);
-          }
-          lastScrollY = scrollY > 0 ? scrollY : 0;
-      };
-      window.addEventListener("scroll", updateScrollDirection);
-      return () => {
-          window.removeEventListener("scroll", updateScrollDirection);
+    let lastScrollY = window.pageYOffset;
+    // function to run on scroll
+    const updateScrollDirection = () => {
+      const scrollY = window.pageYOffset;
+      const direction = lastScrollY > 80 ? "down" : "up";
+      if (direction !== scrollDirection) {
+        setScrollDirection(direction);
       }
+      lastScrollY = scrollY > 0 ? scrollY : 0;
+    };
+    window.addEventListener("scroll", updateScrollDirection);
+    return () => {
+      window.removeEventListener("scroll", updateScrollDirection);
+    };
   }, [scrollDirection]);
 
   return scrollDirection;
-};
-
+}
 
 const Header = () => {
   const scrollDirection = useScrollDirection();
   const [isOpen, setIsOpen] = useState(false);
   const [windowWidth, setWindowWidth] = useState(null);
-  const openMenuBar=()=>{
+  const openMenuBar = () => {
+    ReactGA.event(isOpen, "Toggle Menu");
     setIsOpen(!isOpen);
-  }
+  };
   useEffect(() => {
-    
     document.querySelector("body").classList.remove("no-scroll");
   }, []);
 
@@ -42,50 +43,59 @@ const Header = () => {
     function handleResize() {
       setWindowWidth(window.innerWidth);
     }
-    
-    // Set the initial window width
     setWindowWidth(window.innerWidth);
-
-    // Add an event listener to update the window width whenever it changes
-    window.addEventListener('resize', handleResize);
-
-    // Remove the event listener when the component unmounts
-    return () => window.removeEventListener('resize', handleResize);
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
   }, []);
 
   return (
-    <header className="header " >
+    <header className="header ">
       <div className="header__builder">
-        <div className="logo-image" style={{display: scrollDirection==="down"?"none":"block"}}>
+        <div
+          className="logo-image"
+          style={{ display: scrollDirection === "down" ? "none" : "block" }}
+        >
           <Link legacyBehavior href="/">
             <a>
-              <Image src={drootLogoPic} alt="logo" style={{width:"150px" ,height:"50px"}} className="img"/>
+              <Image
+                src={drootLogoPic}
+                alt="logo"
+                style={{ width: "150px", height: "50px" }}
+                className="img"
+              />
             </a>
           </Link>
         </div>
-        
-          {
-            windowWidth<1100 ? ( <div className="menu-btn"><button  className="btnn"  onClick={openMenuBar}>Menu</button> 
-            <div className="dropdown_menu dropdown_menu--animated  dropdown_menu-8" style={{display : isOpen ?"block":"none"}}>
+
+        {windowWidth < 1100 ? (
+          <div className="menu-btn">
+            <button className="btnn" onClick={openMenuBar}>
+              Menu
+            </button>
+            <div
+              className="dropdown_menu dropdown_menu--animated  dropdown_menu-8"
+              style={{ display: isOpen ? "block" : "none" }}
+            >
               <a href="/">Home</a>
               <a href="/about">Our Story</a>
               <a href="/services">Our Services</a>
               <a href="/work">Work</a>
               <a href="/contact">Contact </a>
             </div>
-            </div>):(
-              <div className="menu-btn"><button  className="btnn">Menu</button> 
-              <div className="dropdown_menu dropdown_menu--animated  dropdown_menu-8">
-                <a href="/">Home</a>
-                <a href="/about">Our Story</a>
-                <a href="/services">Our Services</a>
-                <a href="/work">Work</a>
-                <a href="/contact">Contact</a>
-              </div>
-              </div>
-            )
-          }
-      </div> 
+          </div>
+        ) : (
+          <div className="menu-btn">
+            <button className="btnn">Menu</button>
+            <div className="dropdown_menu dropdown_menu--animated  dropdown_menu-8">
+              <a href="/">Home</a>
+              <a href="/about">Our Story</a>
+              <a href="/services">Our Services</a>
+              <a href="/work">Work</a>
+              <a href="/contact">Contact</a>
+            </div>
+          </div>
+        )}
+      </div>
     </header>
   );
 };
